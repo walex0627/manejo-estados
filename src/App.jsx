cf4017351b1694bf828fe20bd378c7b68b1da3d6
walex0627 <walteralex0627@gmail.com>
2026-01-20 09:56:00 -0500

    2. creacion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { UseState } from './UseState.jsx'
+import { ClassState } from './ClassState.jsx'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <UseState/>
+      <ClassState/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/ClassState.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class ClassState extends React.Component{
+    render(){
+        return (
+        <div className='flex flex-col items-center p-8 bg-gray-200 shadow-lg rounded-xl border border-gray-300 max-w-md mx-auto mt-10'>
+            <h1 className='text-2xl font-bold mb-4 text-gray-800'>Eliminar ClassState</h1>
+            <p className='text-gray-600 mb-6 text-center'>Por favor, escribe el codigo de seguridad</p>
+            <input placeholder='Codigo de seguridad'
+                            className='w-full px-4 py-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all'
+            />
+            <button className='w-full bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors shadow-md'>Comprobar</button>
+        </div>
+        )
+    }
+}
+
+
+export {ClassState}
