87974d8e890cb9a72938f868f479be058dca6193
walex0627 <walteralex0627@gmail.com>
2026-01-21 09:26:33 -0500

    7. Actualizacion de estados en interfaces de usuario en React.

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -13,12 +13,13 @@ function UseState({ name }) {
         if (!!loading) {
             setTimeout(() => {
 
-                if(value !== SECURITY_CODE){
-                    setError(true)
+                if(value === SECURITY_CODE){
+                    setLoading(false)
+                    // setError(true)
                 }else{
-                    setError(false)
+                    setError(true)
+                    setLoading(false)
                 }
-                setLoading(false)
             }, 3000)
         }
     }, [loading])
@@ -27,7 +28,7 @@ function UseState({ name }) {
         <div className='flex flex-col items-center p-8  bg-gray-200 shadow-lg rounded-xl border border-gray-200 max-w-md mx-auto mt-10'>
             <h1 className='text-2xl font-bold mb-4 text-gray-800'>Eliminar {name}</h1>
             <p className='text-gray-600 mb-6 text-center'>Por favor, escribe el codigo de seguridad</p>
-            {error && (
+            {(error && !loading) && (
                 <p> Error: El codigo es incorrecto</p>
             )}
 
@@ -42,7 +43,10 @@ function UseState({ name }) {
                 placeholder='Codigo de seguridad'
                 className='w-full px-4 py-2 border bg-white border-gray-300 rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all'
             />
-            <button onClick={() => setLoading(!loading)} className='w-full bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors shadow-md'>Comprobar</button>
+            <button onClick={() =>{ 
+                setError(false)
+                setLoading(true)
+                }} className='w-full bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors shadow-md'>Comprobar</button>
         </div>
     )
 }
